49604f31dbd0036a726399ced07a45f2176dd434
SOUQUET Thibault <contact@thibaultsouquet.fr>
2021-05-11 15:00:46 +0200

    Email V1

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const nodemailer = require("nodemailer");
 
 const app = express()
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const port = 3000
 const errors = new Map();
 const ok = new Map();
+const transporter = nodemailer.createTransport({
+    host: "thibaultsouquet-fr.mail.protection.outlook.com",
+    port: 25,
+    requireTLS: true,
+    secure: false,
+});
 
 app.post('/', (req, res) => {
     parseBody(req.body)
-    res.send('Hello World!')
+    res.send('done');
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`);
 })
 
 function parseBody(body) {
@@ -30,16 +37,16 @@ function parseBody(body) {
     if(!errors.get(body._check_id) && ok.has(body._check_id) && body._level !== 'ok') {
         errors.set(body._check_id, body);
         ok.delete(body._check_id);
-        sendBotMessage(body);
+        notify(body);
     }
     if(errors.get(body._check_id) && body._level === 'ok') {
         errors.delete(body._check_id);
         ok.set(body._check_id, body);
-        sendBotMessage(body);
+        notify(body);
     }
 }
 
-function sendBotMessage(body) {
+function sendDiscordMessage(body) {
     const config = {
         method: 'post',
         url: `https://discord.com/api/webhooks/${process.env.WEBHOOK_ID}/${process.env.WEBHOOK_SECRET}`,
@@ -55,7 +62,7 @@ function sendBotMessage(body) {
                 description: body._message,
                 footer:{
                     icon_url: 'https://influxdata.github.io/branding/img/downloads/influxdata-logo--symbol--pool-alpha.png',
-                    text: 'InfluxDB 2'
+                    text: 'Notification by SOUQUET Thibault'
                 },
             }],
         }
@@ -65,6 +72,15 @@ function sendBotMessage(body) {
     } catch (e) {
         console.error(e);
     }
+}
+
+function sendMail(body) {
+    transporter.sendMail({
+        from: 'grafana@thibaultsouquet.fr',
+        to: "contact@thibaultsouquet.fr",
+        subject: `${body._check_name} - ${body._level}`,
+        html: `<h1>${body._level}</h1><br /><p>${body._message}</p>`,
+    }).catch()
 }
 
 function getColorFromLevel(code) {
@@ -80,4 +96,9 @@ function getColorFromLevel(code) {
         default:
             return 10067371;
     }
+}
+
+function notify(body) {
+    sendDiscordMessage(body);
+    sendMail(body);
 }
